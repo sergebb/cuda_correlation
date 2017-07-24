Add second part of feature vector in calculation; Fixed bugs for images with huge masked regions; Test compare final feature vectors;

test_lib.py
@@ -4,6 +4,7 @@ import sys
 import time
 import numpy as np
 import scipy as sp
+from scipy.fftpack import dct
 import proj
 import matplotlib.pyplot as plt
 import libcorr
@@ -49,11 +50,12 @@ def RecoverGap(polar_data,polar_mask):
 def main():
     lim = 26
     image_data = []
-    for i in range(1, 300):
+    for i in range(1, 10):
         sys.stderr.write('%d\n' % i)
-        image = np.fromfile('../shuffeled_2/%.04d.raw' % i, \
-                                dtype=np.float32).reshape((IMG_SIZE_Y, IMG_SIZE_X))
-        image[image < 0] = 0
+        # image = np.fromfile('../shuffeled_2/%.04d.raw' % i, \
+                                # dtype=np.float32).reshape((IMG_SIZE_Y, IMG_SIZE_X))
+        image = proj.OpenDiffractionImage('../diffraction-apps/EXP_CXI20-25-37/shuffled/%.04d.npy'%i)
+        # image[image < 0] = 0
         # image[500:540, :] = -10000
         image_data.append(image)
 
@@ -63,9 +65,14 @@ def main():
 
     limit_edge_inner = 0
     limit_edge_outer = min(size_x//4, size_y//4)
+    rad_range = limit_edge_outer - limit_edge_inner
+
     origin = (size_x/2.0, size_y/2.0)
 
     ccf_data = []
+    result_fvs_cpu = []
+    result_fvs_gpu = []
+    rad_data = []
     with Timer() as t:
         for image in image_data:
             mask = np.zeros_like(image)
@@ -81,49 +88,96 @@ def main():
             ccf_a = proj.combine_ccf_for_angles(ccf_2d_np)
             ccf_data.append(ccf_a)
 
+            rad_i = proj.c0_q(considred_data)
+            rad_data.append(rad_i)
+
+            if ccf_a[0] > 0:
+                ccf_a /= ccf_a[0]
+            else:
+                ccf_a[:] = 0
+            dct_a = dct(ccf_a, norm='ortho')[:50] # Ortho don't affect quality
+            dct_a[0] = 0
+
+            dctccf = np.concatenate((dct_a[:50], rad_i))
+            result_fvs_cpu.append(dctccf)
+
+
         ccf_data = np.array(ccf_data)
+        rad_data = np.array(rad_data)
+        
+        # polar_2d_data = np.array(polar_2d_data)
 
     print('Numpy calculation took %.03f sec.' % t.interval)
 
     # cpu_ccf_2d_data = np.array(cpu_ccf_2d_data)
 
-    gpu_ccf_data = np.zeros_like(ccf_data, dtype=np.float32)
+    gpu_ccf_data = np.zeros(500*num_images, dtype=np.float32).reshape(num_images, 500)
+    gpu_rad_data = np.zeros(rad_range*num_images, dtype=np.float32).reshape(num_images, rad_range)
+    
+    # polar_1 = np.zeros_like(considred_data)
+
+    # gpu_polar_2d = np.zeros_like(polar_2d_data,dtype=np.float32)
 
     with Timer() as t:
-        libcorr.ReprojectAndCorrealate(image_data, gpu_ccf_data, origin[1], origin[0], limit_edge_inner, limit_edge_outer, 0)
+        # for i,image in enumerate(image_data):
+            # libcorr.ReprojectToPolar(image, polar_1, origin[1], origin[0], limit_edge_inner, limit_edge_outer, 0)
+            # libcorr.CorrelateLine(polar_1, gpu_polar_2d[i,:,:])
+            # libcorr.ReprojectAndCorrealate(image, gpu_ccf_data[i,:], gpu_rad_data[i,:], origin[1], origin[0], limit_edge_inner, limit_edge_outer, 0)
+        libcorr.ReprojectAndCorrealate(image_data, gpu_ccf_data, gpu_rad_data, origin[1], origin[0], limit_edge_inner, limit_edge_outer, 0)
+        for n, ccf_a in enumerate(gpu_ccf_data):
+            if ccf_a[0] > 0:
+                ccf_a /= ccf_a[0]
+            else:
+                ccf_a[:] = 0
+            dct_a = dct(ccf_a, norm='ortho')[:50] # Ortho don't affect quality
+            dct_a[0] = 0
+            print dct_a[:50]
 
+            rad_i = gpu_rad_data[n, :]
+
+            dctccf = np.concatenate((dct_a[:50], rad_i))
+            result_fvs_gpu.append(dctccf)
     print('Cuda calculation took %.03f sec.' % t.interval)
 
     fig = plt.figure('Comparison')
 
-    # for i in range(num_images):
-    #     ccf_cpu = ccf_data[i, :]
-    #     ccf_gpu = gpu_ccf_data[i, :]
+    for i in range(num_images):
+        ccf_cpu = ccf_data[i, :]
+        ccf_gpu = gpu_ccf_data[i, :]
 
-    #     ccf_cpu /= ccf_cpu[0]
-    #     ccf_gpu /= ccf_gpu[0]
+        ccf_cpu /= ccf_cpu[0]
+        ccf_gpu /= ccf_gpu[0]
 
-    #     fig = plt.figure('Comparison')
-    #     ax1 = fig.add_subplot(1, 3, 1)
-    #     plt.plot(ccf_cpu)
-    #     # plt.imshow(ccf_data)
-    #     # plt.colorbar()
+        result_cpu = result_fvs_cpu[i]
+        result_gpu = result_fvs_gpu[i]
 
-    #     ax2 = fig.add_subplot(1, 3, 2, sharex=ax1)
-    #     plt.plot(ccf_gpu)
-    #     # plt.imshow(gpu_ccf_data)
-    #     # plt.colorbar()
+        fig = plt.figure('Comparison')
+        ax1 = fig.add_subplot(3, 1, 1)
+        plt.plot(result_cpu)
+        # plt.plot(rad_cpu)
+        # plt.imshow(polar_2d_data[i,:,:])
+        # plt.imshow(ccf_data)
+        # plt.colorbar()
 
-    #     ax3 = fig.add_subplot(1, 3, 3, sharex=ax1)
-    #     plt.plot(ccf_cpu-ccf_gpu)
-    #     # plt.imshow(ccf_data-gpu_ccf_data)
-    #     # plt.colorbar()
+        ax2 = fig.add_subplot(3, 1, 2, sharex=ax1)
+        plt.plot(result_gpu)
+        # plt.plot(rad_gpu)
+        # plt.imshow(gpu_polar_2d[i,:,:])
+        # plt.imshow(gpu_ccf_data)
+        # plt.colorbar()
 
-    #     ax1.set_ylim([0, 1])
-    #     ax2.set_ylim([0, 1])
-    #     ax3.set_ylim([0, 1])
+        ax3 = fig.add_subplot(3, 1, 3, sharex=ax1)
+        plt.plot(result_cpu-result_gpu)
+        # plt.plot(rad_cpu-rad_gpu)
+        # plt.imshow(polar_2d_data[i,:,:]-gpu_polar_2d[i,:,:])
+        # plt.imshow(ccf_data-gpu_ccf_data)
+        # plt.colorbar()
 
-    #     plt.show()
+        # ax1.set_ylim([0, 1])
+        # ax2.set_ylim([0, 1])
+        # ax3.set_ylim([0, 1])
+
+        plt.show()
 
 if __name__ == '__main__':
     main()